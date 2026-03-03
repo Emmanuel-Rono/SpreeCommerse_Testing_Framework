39c73e92df212053aa5492d2e3d9a170050d274e
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-03-03 23:48:22 +0300

    worked on configuration module

---
+++ Configuration/settings.py
@@ -0,0 +1,5 @@
+
+
+BASE_URL = "https://demo.spreecommerce.org"
+PRODUCTS_PAGE = f"{BASE_URL}/products"
+IMPLICIT_WAIT =5
---
+++ Configuration/waits.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def wait_for_element_present(driver, locator,timeout):
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located(locator)
+    )
+
+def wait_for_clickable(driver, locator, timeout=10):
+    return WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable(locator)
+    )
