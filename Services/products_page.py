a98de85bd2fd607b6f9aad0da84f9a54d84f9f98
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-03-09 23:43:19 +0300

    added more services

--- Services/products_page.py
+++ Services/products_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 from Configuration.settings import PRODUCTS_PAGE
@@ -21,4 +22,16 @@ class OpenProductsPage(BasePage):
         productlinks=self.driver.find_elements(*ProductsPageLocators.PRODUCT_ITEMS)
         if productlinks:
             productlinks[0].click()
+
+    def  search_products(self):
+        search_box = self.driver.find_element(*ProductsPageLocators.SEARCH_INPUT)
+        search_box.clear()
+        search_box.send_keys(product_name)
+        search_button = self.driver.find_element(*ProductsPageLocators.SEARCH_BUTTON)
+        search_button.click()
+
+    def add_first_product_to_wishlist(self):
+        wishlist_buttons = self.driver.find_elements(*ProductsPageLocators.WISHLIST_BUTTON)
+        if wishlist_buttons:
+            wishlist_buttons[0].click()
 
