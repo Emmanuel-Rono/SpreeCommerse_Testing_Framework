ce11e3f7c7f27bfd9522284deb2336c01d3477c5
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-03-10 16:09:00 +0300

    More services

--- Locators/locators.py
+++ Locators/locators.py
@@ -17,3 +17,10 @@ class ProductsPageLocators:
             By.CSS_SELECTOR,
             f"div[data-wished-item-variant-id-value='{variant_id}'] button[aria-label='Add to wishlist']"
         )
+
+    @staticmethod
+    def products_button(product_id):
+        return (
+            By.CSS_SELECTOR,
+            f"div['mb-3 relative w-full product-card-featured-image'] id='{product_id}']"
+        )
