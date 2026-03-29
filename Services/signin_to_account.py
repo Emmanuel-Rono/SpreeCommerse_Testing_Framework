14664d29b16123cf3611bb7a687e5f07680b7851
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-03-29 11:06:50 +0300

    Added login Service

--- Services/signin_to_account.py
+++ Services/signin_to_account.py
@@ -5,11 +5,19 @@ from Services.basepage import BasePage
 class Customer_Acccount(BasePage):
     def open_account_page(self):
         account_button= self.driver.find_element(By.XPATH, "//*[@id='section-21057']/header/nav/div/div/div[3]/div[2]/button")
-
+        try:
+            account_button.click()
+        except Exception as e:
+            print(f"Error:{e}")
+    
     def specify_size():
 
-    def specify_quantity():
 
+    def specify_quantity():
+    
+    
+
+#Add to cart
     def add_to_cart():
 
     
