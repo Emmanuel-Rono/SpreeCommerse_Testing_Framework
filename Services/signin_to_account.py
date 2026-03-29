fafbcf2b3a41d67ed4cda20087811e2a5c49fd58
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-03-29 11:04:10 +0300

    Added login Service

---
+++ Services/signin_to_account.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+from Services.basepage import BasePage
+
+class Customer_Acccount(BasePage):
+    def open_account_page(self):
+        account_button= self.driver.find_element(By.XPATH, "//*[@id='section-21057']/header/nav/div/div/div[3]/div[2]/button")
+
+    def specify_size():
+
+    def specify_quantity():
+
+    def add_to_cart():
+
+    
