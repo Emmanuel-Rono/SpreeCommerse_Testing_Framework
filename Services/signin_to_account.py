278ec746f0ea7c53a98b0dd863c852dc070b8a52
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-04-02 23:36:25 +0300

    Login service

--- Services/signin_to_account.py
+++ Services/signin_to_account.py
@@ -5,22 +5,12 @@ from Services.basepage import BasePage
 class Customer_Acccount(BasePage):
     def open_account_page(self):
         account_button= self.driver.find_element(By.XPATH, "//*[@id='section-21057']/header/nav/div/div/div[3]/div[2]/button")
+        enter_password_field = self.driver.find_element(By.XPATH, '//*[@id="email']")
         try:
             account_button.click()
+            assert enter_password_field.is_displayed()
+            self.driver.implicitly_Wait(5)
         except Exception as e:
             print(f"Error:{e}")
     
-    def specify_size(self):
-        pass
 
-
-    def specify_quantity():
-        pass
-    
-    
-
-#Add to cart
-    def add_to_cart():
-        pass
-
-    
