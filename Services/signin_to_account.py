d0153738177327f7f79cd8b2148af484f349e2b4
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-04-07 23:49:31 +0300

    Working on auth tests

--- Services/signin_to_account.py
+++ Services/signin_to_account.py
@@ -10,51 +10,52 @@ class Customer_Account(BasePage):
     enter_password_field_locator = (By.XPATH, '//*[@id="password"]')
     sign_in_button_locator=(By.XPATH, '/html/body/main/div[1]/div/div[2]/form/div[4]/button')
     account_button_after_sign_locator = (By.XPATH, '/html/body/header/div[1]/div/div/div[3]/div[2]/a')
-    account_signed_in_fields_locator =(By.XPATH, '/html/body/main/div[1]/div/aside/div/nav/ul') 
+    account_signed_in_fields_locator =(By.XPATH, '/html/body/main/div[1]/div/aside/div/nav/ul')
+    login_error_locator = (By.XPATH, "//*[contains(@class,'error') or contains(@class,'alert')]")
 
-    def open_account_page(self):
-    
-            account_button = WebDriverWait(self.driver, timeout=10).until(self.driver.find_element(self.account_button_locator))
-            enter_email_field = WebDriverWait(self.driver,timeout=10).until(self.driver.find_element(self.enter_email_field_locator))
-            account_button.clik()
-            return enter_email_field.is_displayed() 
-    
-#To enter the login credentials in the fields
-    def enter_login_credentials(self, email,password,timeout=10):
-            enter_email_field = WebDriverWait(self.driver,timeout).untill(
-                 EC.visibility_of_element_located(self.find_element(
-                      self.enter_email_field_locator)))
-                                                                                                                
-            enter_password_field = WebDriverWait(self.driver, timeout=10).untill(
-                 EC.visibility_of_element_located(self.find_element(
-                      self.enter_password_field_locator)))
-        
-            sign_in_button= WebDriverWait(self.driver,timeout=10).until(
-                 EC.visibility_of_element_located(self.find_element(self.sign_in_button_locator))
+    def open_account_page(self, timeout=10):
+        account_button = WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(self.account_button_locator)
+        )
+        account_button.click()
+        enter_email_field = WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(self.enter_email_field_locator)
+        )
+        return enter_email_field.is_displayed()
+
+    # To enter the login credentials in the fields
+    def enter_login_credentials(self, email, password, timeout=10):
+        enter_email_field = WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(self.enter_email_field_locator)
+        )
+        enter_password_field = WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(self.enter_password_field_locator)
+        )
+        sign_in_button = WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(self.sign_in_button_locator)
+        )
+        enter_email_field.clear()
+        enter_email_field.send_keys(email)
+        enter_password_field.clear()
+        enter_password_field.send_keys(password)
+        sign_in_button.click()
+        return True
+
+    def is_logged_in(self, timeout=10):
+        account_button_after_signin = WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(self.account_button_after_sign_locator)
+        )
+        account_button_after_signin.click()
+        account_signed_in_fields = WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(self.account_signed_in_fields_locator)
+        )
+        return account_signed_in_fields.is_displayed()
+
+    def has_login_error(self, timeout=5):
+        try:
+            error_elem = WebDriverWait(self.driver, timeout).until(
+                EC.visibility_of_element_located(self.login_error_locator)
             )
-            enter_email_field.clear()
-            enter_email_field.send_keys(email)
-            enter_password_field.clear()
-            enter_password_field.send_keys(password)
-            sign_in_button.click()
-            return True
-
-    def is_logged_in(self):
-
-            account_button_after_signin=WebDriverWait(self.driver,timeout=10).until(
-                 EC.visibility_of_element_located(self.find_element(self.account_button_after_sign_locator))
-            )
-            account_signed_in_fields= WebDriverWait(self.driver,timeout=10).until(
-                  EC.visibility_of_element_located(self.account_signed_in_fields_locator(self.find_element(self.account_signed_in_fields_locator))
-            ))
-            account_button_after_signin.click()
-
-            return account_signed_in_fields.is_displayed()
-            
-     
-            
-        
-
-
-    
-
+            return error_elem.is_displayed()
+        except Exception:
+            return False
--- Tests/ui/test_sign_in.py
+++ Tests/ui/test_sign_in.py
@@ -1,5 +1,6 @@
 
 
+from Services.products_page import OpenProductsPage
 from Services.signin_to_account import Customer_Account
 import pytest
 
@@ -7,6 +8,9 @@ import pytest
 @pytest.mark.smoke
 
 def test_open_account_page(driver):
+    products_page =OpenProductsPage(driver)
+    products_page.open_products_page(driver)
     account = Customer_Account(driver)
+    account.open_account_page
     assert account.open_account_page() == True
  
