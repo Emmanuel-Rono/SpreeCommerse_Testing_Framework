6a2bda192a081425bb8f78d538d9570a630763a9
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-04-15 06:01:39 +0300

    Refactored products_page service

--- Services/basepage.py
+++ Services/basepage.py
@@ -1,9 +1,11 @@
 class BasePage:
-    def __init__(self, driver):
+    def __init__(self, driver,settings):
         self.driver = driver
+        self.settings = settings
 
-    def open(self,url):
-            self.driver.get(url)
+    def open(self, path= ""):
+         url = f"{self.settings.base_url}{path}"
+         self.driver.get(url)
 
     def get_title(self):
         return self.driver.title
