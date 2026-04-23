7f506ff0f447afb91f1eea12049ff1d52c27bc74
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-04-24 00:42:38 +0300

    Refactored cartpage

--- Services/cart_page.py
+++ Services/cart_page.py
@@ -6,19 +6,19 @@ from Services.basepage import BasePage
 
 
 class CartPage(BasePage):
-    CART_LINK_CANDIDATES = By.Xpath,"/html/body/header/div[1]/div/div/div[3]/button[2]"
+    TO_CART_BUTTON = (By.XPATH,"/html/body/header/div[1]/div/div/div[3]/button[2]")
 
-    CART_ITEM_CANDIDATES =By.Xpath, "//*[@id="radix-_R_snpffb_"]/div[2]/ul/li"
+    CART_ITEMS = ( By.XPATH, "//ul/li[.//a[contains(@href,'/products/')]]")
+
 
     def open_cart(self, timeout=10):
-        for locator in self.CART_LINK_CANDIDATES:
+        
             try:
-                link = wait_for_clickable(self.driver, locator, timeout=timeout)
+                link = wait_for_clickable(self.driver, timeout=timeout)
                 link.click()
                 return
             except Exception:
-                continue
-        raise AssertionError("Could not find cart link/icon to open cart")
+                raise AssertionError("Could not find cart link/icon to open cart")
 
     def is_cart_page(self):
         url = self.driver.current_url.lower()
@@ -26,7 +26,7 @@ class CartPage(BasePage):
         return "/cart" in url or "cart" in title
 
     def get_line_items_count(self):
-        for locator in self.CART_ITEM_CANDIDATES:
+        for locator in self.TO_CART_BUTTON:
             items = self.driver.find_elements(*locator)
             if items:
                 return len(items)
@@ -34,10 +34,10 @@ class CartPage(BasePage):
 
     def wait_for_line_items(self, timeout=10):
         def _has_items(driver):
-            for locator in self.CART_ITEM_CANDIDATES:
-                items = driver.find_elements(*locator)
+           
+                items = driver.find_elements(*self.CART_ITEMS)
                 if items:
                     return items
-            return False
+           
 
         return WebDriverWait(self.driver, timeout).until(_has_items)
